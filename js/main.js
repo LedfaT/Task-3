8f3b6e28cdd0ab96c18f3c8e7fa67fab84373828
David Zaitsev <zaitsevdavid4@gmail.com>
2024-12-15 11:39:46 +0200

    fix: fixed a bug with error message at start

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ class App {
   #parentEl = document.querySelector(".main__table").querySelector("tbody");
   #form = document.querySelector(".form");
   #recoveryBtn = this.#form.querySelector(".recover-btn");
+  #dataOnPage = 0;
   #input = this.#form.querySelector("input");
   #saved = [];
   #errMessasge = "No country found!!! Try again.";
 
   constructor() {
+    this.#getLocalStorage();
     this.#save();
     this.#recoverData();
     this.#getCountry();
@@ -24,9 +26,16 @@ class App {
       if (!e.target.matches("input") || !this.#data) return;
 
       const index = +e.target.closest("tr").querySelector("th").textContent - 1;
+      if (e.target.checked !== true) {
+        this.#saved.splice(index, 1);
+        this.#setLocalStorage();
+
+        return;
+      }
+
       this.#data[index].checked = true;
-      if (e.target.checked === true) this.#saved.push(this.#data[index]);
-      console.log(this.#saved);
+      this.#saved.push(this.#data[index]);
+      this.#setLocalStorage();
     });
   }
 
@@ -34,6 +43,7 @@ class App {
     const data = JSON.parse(localStorage.getItem("saved"));
     if (!data) return;
     this.#saved = data;
+    this.#renderSaved();
   }
 
   #setLocalStorage() {
@@ -45,6 +55,8 @@ class App {
       e.preventDefault();
       this.#input.value = "";
       this.#parentEl.innerHTML = "";
+      this.#dataOnPage = 0;
+      this.#renderSaved();
     });
   }
 
@@ -52,18 +64,72 @@ class App {
     this.#form.addEventListener("submit", (e) => {
       e.preventDefault();
       const country = this.#input.value;
+      this.#dataOnPage = 0;
+
       if (!country) {
         this.#renderError();
         return;
       }
-      this.render(country);
+
+      this.bringData(country);
+      this.#renderSaved();
+      this.#renderUni();
     });
   }
 
-  render = async (cont) => {
+  #renderSaved() {
+    const saved = this.#saved
+      .map((uni, i) => {
+        this.#dataOnPage++;
+        return `
+                          <tr>
+                              <th scope="row">${this.#dataOnPage}</th>
+                              <td>${uni.country}</td>
+                              <td>${uni.name}</td>
+                              <td>
+                              <a href="${uni.web_pages[0]}">${uni.web_pages}</a>
+                           </td>
+                              <td>${uni.alpha_two_code}</td>
+                              <td>${uni.domains[0]}</td>
+                               <td><input type="checkbox" checked/></td>
+                          </tr>
+                      `;
+      })
+      .join("");
+    this.#parentEl.innerHTML = "";
+    this.#parentEl.insertAdjacentHTML("beforeend", saved);
+  }
+
+  #renderUni() {
+    const HTML = this.#data
+      .map((uni, i) => {
+        this.#dataOnPage++;
+        return `
+                        <tr>
+                            <th scope="row">${this.#dataOnPage}</th>
+                            <td>${uni.country}</td>
+                            <td>${uni.name}</td>
+                            <td>
+                            <a href="${uni.web_pages[0]}">${uni.web_pages}</a>
+                         </td>
+                            <td>${uni.alpha_two_code}</td>
+                            <td>${uni.domains[0]}</td>
+                             <td><input type="checkbox" /></td>
+                        </tr>
+                    `;
+      })
+      .join("");
+
+    this.#parentEl.insertAdjacentHTML("beforeend", HTML);
+  }
+
+  bringData = async (cont) => {
     try {
       const universities = await getJSON(`${API_URL}=${cont}`);
       this.#data = universities;
+
+      this.#data = this.#data.filter((el) => el.name !== this.#saved.name);
+
       this.#data.forEach((el) => {
         el.checked = false;
       });
@@ -72,36 +138,11 @@ class App {
         this.#renderError();
         return;
       }
-
-      const HTML = universities
-        .map(
-          (uni, i) => `
-                            <tr>
-                                <th scope="row">${i + 1}</th>
-                                <td>${uni.country}</td>
-                                <td>${uni.name}</td>
-                                <td>
-                                <a href="${uni.web_pages[0]}">${
-            uni.web_pages
-          }</a>
-                             </td>
-                                <td>${uni.alpha_two_code}</td>
-                                <td>${uni.domains[0]}</td>
-                                 <td><input type="checkbox" /></td>
-                            </tr>
-                        `
-        )
-        .join("");
-
-      this.#parentEl.innerHTML = "";
-      this.#parentEl.insertAdjacentHTML("beforeend", HTML);
-    } catch (err) {
-      console.log(err);
-    }
+    } catch (err) {}
   };
 
   #renderError() {
-    const err = ` <p class="error">${this.#errMessasge}</p>`;
+    const err = <p class="error">${this.#errMessasge}</p>;
     console.log(err);
     this.#parentEl.innerHTML = "";
     this.#parentEl.insertAdjacentHTML("beforeend", err);
